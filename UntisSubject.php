cacfbc0c8db79b5bbf45abb6e0060fc801183d37
craftamap <fabians1999@gmail.com>
2016-11-24 09:27:38 +0100

    Added UntisSubject for getSubjects

--- UntisSubject.php
+++ UntisSubject.php
@@ -1,6 +1,24 @@
 <?php
 class UntisSubject {
-  
+  public $id;
+  public $name;
+  public $longName;
+  public $foreColor;
+  public $backColor;
+  public $alternateName;
+  public $active;
+
+  function __construct($ar){
+    if(is_array($ar)) {
+      $this->id = $ar["id"];
+      $this->name = $ar["name"];
+      $this->longName = $ar["longName"];
+      $this->foreColor = $ar["foreColor"];
+      $this->backColor = $ar["backColor"];
+      $this->alternateName = $ar["alternateName"];
+      $this->active = $ar["active"];
+    }
+  }
 }
 
 
